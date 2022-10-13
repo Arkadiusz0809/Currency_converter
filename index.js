e6af2b9da20dcd0d6cf889ef7e49260e54232e40
Arkadiusz Torun <arek.torun12@gmail.com>
2022-10-13 20:24:38 +0200

    refactoring js

--- index.js
+++ index.js
@@ -1,112 +1,131 @@
-let formElement = document.querySelector(".js-form");
-let valueElement = document.querySelector(".js-value1");
-let inputCurrencyElement = document.querySelector(".js-inputCurrency");
-let outputCurrencyElement = document.querySelector(".js-outputCurrency");
-let resultElement = document.querySelector(".js-result");
+{
+    
+    CalculateResult = (inputCurrency, outputCurrency, amount) => {
 
-valueElement.focus();
+        const pln = 1;
+        const eur = 4.86;
+        const usd = 4.95;
+        const gbp = 5.52;
 
-let pln = 1;
-let eur = 4.86;
-let usd = 4.95;
-let gbp = 5.52;
+        switch (inputCurrency) {
+            case "EUR":
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let inputCurrency = inputCurrencyElement.value;
-    let outputCurrency = outputCurrencyElement.value;
-    let amount = valueElement.value;
-
-    let result;
-
-    switch (inputCurrency) {
-        case "EUR":
-
-            switch (outputCurrency) {
-                case "GBP":
-                    result = (amount * eur) / gbp;
-                    break;
-
-                case "EUR":
-                    result = amount * 1;
-                    break;
-
-                case "USD":
-                    result = (amount * eur) / usd;
-                    break;
-
-                case "PLN":
-                    result = (amount * eur) / pln;
-                    break;
-            }
-            break;
-
-        case "GBP":
-
-            switch (outputCurrency) {
-                case "EUR":
-                    result = (amount * gbp) / eur;
-                    break;
-
-                case "GBP":
-                    result = amount * 1;
-                    break;
-
-                case "USD":
-                    result = (amount * gbp) / usd;
-                    break;
-
-                case "PLN":
-                    result = (amount * gbp) / pln;
-                    break;
-            }
-            break;
-
-        case "USD":
-
-            switch (outputCurrency) {
-                case "EUR":
-                    result = (amount * usd) / eur;
-                    break;
-
-                case "GBP":
-                    result = (amount * usd) / gbp;
-                    break;
-
-                case "USD":
-                    result = amount * 1;
-                    break;
-
-                case "PLN":
-                    result = (amount * usd) / pln;
-                    break;
-            }
-            break;
-
-        case "PLN":
-
-            switch (outputCurrency) {
-                case "EUR":
-                    result = (amount * pln) / eur;
-                    break;
-
-                case "GBP":
-                    result = (amount * pln) / gbp;
-                    break;
-
-                case "USD":
-                    result = (amount * pln) / usd;
-                    break;
-
-                case "PLN":
-                    result = amount * 1;
-                    break;
-            }
-            break;
-    }
-    resultElement.innerHTML = ` ${result.toFixed(2)} ${outputCurrencyElement.value}`;
-});
+                switch (outputCurrency) {
+                    case "GBP":
+                        return (amount * eur) / gbp;
 
 
+                    case "EUR":
+                        return amount * 1;
 
+
+                    case "USD":
+                        return (amount * eur) / usd;
+
+
+                    case "PLN":
+                        return (amount * eur) / pln;
+
+                }
+
+
+            case "GBP":
+
+                switch (outputCurrency) {
+                    case "EUR":
+                        return (amount * gbp) / eur;
+
+
+                    case "GBP":
+                        return amount * 1;
+
+
+                    case "USD":
+                        return (amount * gbp) / usd;
+
+
+                    case "PLN":
+                        return (amount * gbp) / pln;
+
+                }
+
+
+            case "USD":
+
+                switch (outputCurrency) {
+                    case "EUR":
+                        return (amount * usd) / eur;
+
+
+                    case "GBP":
+                        return (amount * usd) / gbp;
+
+
+                    case "USD":
+                        return amount * 1;
+
+
+                    case "PLN":
+                        return (amount * usd) / pln;
+
+                }
+
+
+            case "PLN":
+
+                switch (outputCurrency) {
+                    case "EUR":
+                        return (amount * pln) / eur;
+
+
+                    case "GBP":
+                        return (amount * pln) / gbp;
+
+
+                    case "USD":
+                        return (amount * pln) / usd;
+
+
+                    case "PLN":
+                        return amount * 1;
+
+                }
+
+        };
+    };
+    const updateResultText = (result, outputCurrency) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerHTML = `${result.toFixed(2)} ${outputCurrency}`;
+    };
+
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+
+        const valueElement = document.querySelector(".js-value1");
+        const inputCurrencyElement = document.querySelector(".js-inputCurrency");
+        const outputCurrencyElement = document.querySelector(".js-outputCurrency");
+
+
+        const inputCurrency = inputCurrencyElement.value;
+        const outputCurrency = outputCurrencyElement.value;
+        const amount = valueElement.value;
+        
+
+        let result = CalculateResult(amount, outputCurrency, inputCurrency);
+
+        updateResultText(result, outputCurrency);
+    };
+
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        
+
+        formElement.addEventListener("submit", onFormSubmit);
+        formElement.focus();
+    };
+
+    init();
+}
